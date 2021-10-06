2c72c6ca5960158723638bad29df83be68d7577d
siobhanschenk <sschenk@uwo.ca>
2021-10-06 13:44:25 -0700

    Added data and initial code

---
+++ GW_seaweed_seasonality_analysis.R
@@ -0,0 +1,28 @@
+## load packages
+library(tidyverse)
+library(ggplot2); theme_set(theme_bw()+
+                              theme(panel.grid = element_blank(),
+                                    strip.background = element_rect(fill="white"),
+                                    axis.text = element_text(size = 12, color="black"), 
+                                    axis.title = element_text(size=15, color="black")))
+
+## set working directory
+setwd("C:/Users/siobh/OneDrive - The University Of British Columbia/Project - Seaweed Seasonality Transects/seaweed_seasonality_2021-09-05/git_GW_seaweed_seasonality_transects")
+
+## read in data
+algae = read.csv("GW_seaweed_seasonality_transect_data.csv")
+
+
+## dot-line plot (fucus)
+ggplot(algae, aes(x=distance_along_transect, y=fucus_distichus_percent.cover, 
+                  shape=sampling_date))+
+  geom_point(cex=3)+
+  geom_line()+
+  facet_grid(transect_id~.)
+
+## dot-line plot (ulav)
+ggplot(algae, aes(x=distance_along_transect, y=ulva_fenestrata_percent.cover, 
+                  shape=sampling_date))+
+  geom_point(cex=3)+
+  geom_line()+
+  facet_grid(transect_id~.)
